cfe427da4ce35abfa4eb755f7181f61adf9a4959
collin-thomas <collin.inbox@gmail.com>
2021-05-11 21:30:01 -0400

    fixed

---
+++ README.md
@@ -0,0 +1,11 @@
+# README
+
+## Run app
+
+This will start the application on port 1337.
+
+You can access the the web app and API from the same port.
+
+```shell
+node .
+```
--- index.js
+++ index.js
@@ -1,26 +1,36 @@
-const express = require('express')
-const app = express()
-app.use(express.json())
-const port = 3000
+const path = require("path");
+const express = require("express");
+const app = express();
+app.use(express.json());
+const port = 1337;
 
 // store contacts in an array
-const contacts = []
+const contacts = [];
+const products = [];
 
-app.get('/', (req, res) => {
-  res.send("<h1>Routes: Try a GET request to /contacts and a POST request to /contact</h1>")
-})
+/* app.get("/", (req, res) => {
+  res.send(
+    "<h1>Routes: Try a GET request to /contacts and a POST request to /contact</h1>"
+  );
+});
+ */
+app.use("/", express.static("public"));
 
 // list all contacts
-app.get('/contacts', (req, res) => {
-  res.json(contacts)
-})
+app.get("/api/contact", (req, res) => {
+  res.json(contacts);
+});
 
 // add a contact
-app.post('/contact', (req, res) => {
-  contacts.push({ name: req.body.name, email: req.body.email })
-  res.json(req.body)
-})
+app.post("/api/contact", (req, res) => {
+  contacts.push({ name: req.body.name, email: req.body.email });
+  res.json(req.body);
+});
+
+app.get("/api/product", (req, res) => {
+  res.json(products);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
